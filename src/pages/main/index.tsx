0897aedb28be43af8992121c6f63f69a8771aa80
John <john@John.local>
2018-08-28 12:16:45 +0800

    修改tabbar item属性

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,20 @@
 import { TabBar } from '../lib/antd'
-import './styles/tabbar.less'
 import { Hot, Ships, Profile } from './constant'
 import { Icon } from '../lib/FontIcon'
 
 interface MainPropsState {
 	selectedTab: string,
 	hidden: boolean,
-	fullScreen: boolean
+	fullScreen: boolean,
+	itemBadges: { [key: string]: number },
+	itemClicks: { [key: string]: () => void },
+	itemHots: { [key: string]: boolean }
+}
+
+enum ItemKey {
+	hot = 'hot',
+	ships = 'ships',
+	profile = 'profile'
 }
 
 export default class Main extends Component<{}, MainPropsState>{
@@ -14,16 +22,31 @@ export default class Main extends Component<{}, MainPropsState>{
 	constructor(props: any) {
 		super(props)
 		this.state = {
-			selectedTab: 'hot',
+			selectedTab: ItemKey.hot,
 			hidden: false,
-			fullScreen: true
+			fullScreen: true,
+			itemBadges: { hot: 0, ships: 1, profile: 0 },
+			itemHots: { hot: false, ships: false, profile: true },
+			itemClicks: { hot: this.hotClick, ships: this.shipsClick, profile: this.profileClick }
 		}
+	}
+
+	hotClick() {
+		console.log('hotClick');
+	}
+
+	shipsClick() {
+		console.log('shipsClick');
+	}
+
+	profileClick() {
+		console.log('profileClick');
 	}
 
 	renderTabBar() {
 		// 这里放一些东西
 		let titles = ['热点', '船舶', '个人']
-		let keys = ['hot', 'ships', 'profile']
+		let keys = [ItemKey.hot, ItemKey.ships, ItemKey.profile]
 		let components = [Hot, Ships, Profile]
 		let icons = ['icon-redian2', 'icon-lunchuan', 'icon-my']
 
@@ -42,12 +65,13 @@ export default class Main extends Component<{}, MainPropsState>{
 							icon={<Icon name={icons[index]} size={24} color={'gray'} />}
 							selectedIcon={<Icon name={icons[index]} size={24} color={'rgb(51, 163, 244)'} />}
 							selected={this.state.selectedTab === keys[index]}
-							badge={0}
-							dot={false}
+							badge={this.state.itemBadges[keys[index]]}
+							dot={this.state.itemHots[keys[index]]}
 							onPress={() => {
 								this.setState({
 									selectedTab: keys[index],
 								});
+								this.state.itemClicks[keys[index]]()
 							}}
 						>
 							<Component title={titles[index]} />
